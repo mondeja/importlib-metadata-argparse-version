d3513566e4ea75008cdeb30dfc00fa3f692e0800
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2023-11-16 00:51:20 +0100

    Drop unused code

--- src/importlib_metadata_argparse_version/__init__.py
+++ src/importlib_metadata_argparse_version/__init__.py
@@ -38,18 +38,17 @@ class ImportlibMetadataVersionAction(argparse._VersionAction):
         **kwargs,
     ) -> None:
         """Executed when the version option is passed to the CLI."""
-        version = self.version
-        if version is None:
-            # prevents default argparse behaviour because version is optional:
-            # https://github.com/python/cpython/blob/86a5e22dfe77558d2e2609c70d1d9e27274a63c0/Lib/argparse.py
-            #
-            # if version not passed raises here:
-            # AttributeError: 'ArgumentParser' object has no attribute 'version'
-            try:
-                version = parser.version  # type: ignore[attr-defined]
-            except AttributeError:
-                # use '%(version)s' as default placeholder
-                version = '%(version)s'
+        # prevent default argparse behaviour because version is optional:
+        # https://github.com/python/cpython/blob/86a5e22dfe77558d2e2609c70d1d9e27274a63c0/Lib/argparse.py
+        #
+        # if version not passed raises here:
+        # AttributeError: 'ArgumentParser' object has no attribute 'version'
+        try:
+            version = parser.version  # type: ignore[attr-defined]
+        except AttributeError:
+            # use '%(version)s' as default placeholder
+            version = '%(version)s'
+
         if '%(version)s' not in version:
             raise ValueError(
                 "Missing '%(version)s' placeholder in"
